6a5cf4d2eee3bff64feac225d1f761b5af988870
vova& <anhhqse05534@fpt.edu.vn>
2018-04-08 11:06:06 +0700

    no message

--- wikipedia-viewer-20180407T174443Z-001/wikipedia-viewer/index.js
+++ wikipedia-viewer-20180407T174443Z-001/wikipedia-viewer/index.js
@@ -3,35 +3,42 @@ $(document).ready(() => {
 })
 // 
 
-function deb(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply();
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply();
-	};
+function deb(func, wait) {
+    let timeout;
+    // return function() {
+    // 	// const later = function() {
+    //     //         func();
+    //     // }
+    // 	// clearTimeout(timeout);
+    //     // timeout = setTimeout(later, wait);
+    //     clearTimeout(timeout);
+    //     timeout = setTimeout(() => {
+    //         func()
+    //     }, wait);
+
+    // };
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func()
+        }, wait);
+    }
 };
 const listenToInputEvent = () => {
     // Tạo ra 1 function search mới gọi tối 1s 1 lần 
     //const throttledSearch = _.debounce(search, 500)
-    const throttledSearch = deb(search, 500 );
-    
+    const throttledSearch = deb(search, 500);
     const inputElement = $("#article-search-form__input");
     inputElement.on("input", () => {
-        
+
         clearData()
         throttledSearch()
-        
+
     })
 }
 
 function clearData() {
-    
+
     $("div.article-list").empty()
 }
 
@@ -41,11 +48,11 @@ async function search() {
 
     // Check từ khoá search có giống với từ khoá hiện tại của user không
     if (searchQuery != getUserSearchQuery()) {
-        
+
         return // Không chạy bên dưới nữa
     }
-    
-    
+
+
     processData(data)
 }
 
@@ -56,29 +63,29 @@ function getUserSearchQuery() {
 }
 
 async function searchWiki(query) {
-    if(query.length === 0) return
+    if (query.length === 0) return
     $(".loader").hide();
-    
+
     return await $.ajax({
-        url: "https://en.wikipedia.org/w/api.php",
-        data: { // cấu trúc data do server quy định 
-            action: "query",
-            list: "search",
-            format: "json",
-            srprop: "snippet",
-            origin: "*",
-            srsearch: encodeURI(query) // Thay dấu cách = "%20"
-        }
-        // success: processData
-        
-    },
-    $(".article-search-form").append(` <div class="loader " style="margin-left:48%"></div> `),
-)
+            url: "https://en.wikipedia.org/w/api.php",
+            data: { // cấu trúc data do server quy định 
+                action: "query",
+                list: "search",
+                format: "json",
+                srprop: "snippet",
+                origin: "*",
+                srsearch: encodeURI(query) // Thay dấu cách = "%20"
+            }
+            // success: processData
+
+        },
+        $(".article-search-form").append(` <div class="loader " style="margin-left:48%"></div> `),
+    )
 
 }
 
 function processData(data) {
-    
+
     // Hàm này được gọi sau khi dữ liệu được server trả về
 
     /**
@@ -100,18 +107,19 @@ function processData(data) {
     if (!(data.query && data.query.search)) {
         return
     }
-    
+
     // map: chuyển từ array article thành array các thẻ <a>
     // function truyền vào nhận 1 article và return 1 thẻ <a>
+    if (data.query.search.length === 0) $(".article-list").html(`<h3 class="text-center">No result founded</h3>`)
     const elementString = data.query.search.map(article =>
         `<a href="https://en.wikipedia.org/?curid=${article.pageid}" target="_blank" class="article-view">
             <h3 class="article-view__title">${article.title}</h3>
             <p class="article-view__snippet">${article.snippet}</p> 
         </a>`,
-        
+
     ).join("") // Ghép array string lại thành 1 string
 
     $(".article-list").append(elementString); // Đưa các thẻ <a> vừa tạo vào trong div.article-list
     $(".loader").hide();
-    
+
 }
